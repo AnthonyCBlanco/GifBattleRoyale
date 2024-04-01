96836027714a008d5e876829b54b86ec5773b585
Anthony Blanco <anthony200527@outlook.com>
2024-04-01 12:54:31 -0700

    Prompt model and gif schema!

---
+++ server/models/Gif.js
@@ -0,0 +1,16 @@
+const { Schema } = require('mongoose')
+
+const gifSchema = new Schema(
+    {
+        endpoint: {
+            type: String,
+            required: true,
+            match: [/(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/, 'Must Be A Valid URL']
+        },
+        votes: {
+            type: Int
+        }
+    }
+)
+
+module.exports = gifSchema
