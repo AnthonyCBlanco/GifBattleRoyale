1f399040c913c4a4419dbda3f45452e19317c782
gblow <144559335+gblow@users.noreply.github.com>
2024-04-04 20:20:57 -0700

    Update endpoints to endpoint

--- server/seed/seedData.js
+++ server/seed/seedData.js
@@ -4,19 +4,19 @@ const promptsData = [
         text: "You just got in a car accident. What is your reaction?",
         gifs: [
 
-                endpoints: "https://tenor.com/v4eC.gif",
+                endpoint: "https://tenor.com/v4eC.gif",
                 caption: "You better have insurance"
             },
             {
-                endpoints: "https://tenor.com/bhyYs.gif",
+                endpoint: "https://tenor.com/bhyYs.gif",
                 caption: "I just got the car"
             },
             {
-                endpoints: "https://tenor.com/rjRqnVptBor.gif",
+                endpoint: "https://tenor.com/rjRqnVptBor.gif",
                 caption: "RUN!... I don't have insurance"
             },
             {
-                endpoints: "https://tenor.com/bslXo.gif",
+                endpoint: "https://tenor.com/bslXo.gif",
                 caption: "Try to fix it before your parents see"
     }
         ]
@@ -25,20 +25,20 @@ const promptsData = [
         text: "You just get in from a hard working day of being YOU, and your Mom tells you to unload the dishwasher. What do you do?",
         gifs: [
             {
-                endpoints: "https://tenor.com/bKNxf.gif",
+                endpoint: "https://tenor.com/bKNxf.gif",
                 caption: "Throw a secret tantrum"
 
             },
             {
-                endpoints: "https://tenor.com/bucC1.gif",
+                endpoint: "https://tenor.com/bucC1.gif",
                 caption: "Unload the dishwasher because your Mom is the best"
             },
             {
-                endpoints: "https://tenor.com/5VVB.gif",
+                endpoint: "https://tenor.com/5VVB.gif",
                 caption: "Say no"
             },
             {
-                endpoints: "https://tenor.com/Zvtz.gif",
+                endpoint: "https://tenor.com/Zvtz.gif",
                 caption: "Tell one of your siblings to do it"
             },
         ]
@@ -62,19 +62,19 @@ const promptsData = [
         text: "You’re starving and you only have $5.75 in your bank account. What do you do?",
         gifs: [
             {
-                endpoints: "https://tenor.com/bmbWO.gif",
+                endpoint: "https://tenor.com/bmbWO.gif",
                 caption: "Catch that overdraft fee"
             },
             {
-                endpoints: "https://tenor.com/uJvJ.gif",
+                endpoint: "https://tenor.com/uJvJ.gif",
                 caption: "Wait  until you go home to eat"
             },
             {
-                endpoints: "https://tenor.com/WzA0.gif",
+                endpoint: "https://tenor.com/WzA0.gif",
                 caption: "Ask someone to borrow $20 until tomorrow"
             },
             {
-                endpoints: "https://tenor.com/ZA1y.gif",
+                endpoint: "https://tenor.com/ZA1y.gif",
                 caption: "Dollar menu here I come"
             },
         ]
@@ -83,19 +83,19 @@ const promptsData = [
         text: "You just won the lottery. What do you do first?",
         gifs: [
             {
-                endpoints: "https://tenor.com/uRdz.gif",
+                endpoint: "https://tenor.com/uRdz.gif",
                 caption: "Disappear"
             },
             {
-                endpoints: "https://tenor.com/bD7I3.gif",
+                endpoint: "https://tenor.com/bD7I3.gif",
                 caption: "We made it"
             },
             {
-                endpoints: "https://tenor.com/bCctm.gif",
+                endpoint: "https://tenor.com/bCctm.gif",
                 caption: "Quit your job"
             },
             {
-                endpoints: "https://tenor.com/bdS2J.gif",
+                endpoint: "https://tenor.com/bdS2J.gif",
                 caption: "Keep it a secret"
             },
         ]
@@ -104,19 +104,19 @@ const promptsData = [
         text: "You are offered $50,000 to go without bathing, brushing your teeth, or using any cosmetic or cleansing products. What do you do?",
         gifs: [
             {
-                endpoints: "https://tenor.com/dSqt6qEZibA.gif",
+                endpoint: "https://tenor.com/dSqt6qEZibA.gif",
                 caption: "Put the money in my hand"
             },
             {
-                endpoints: "https://tenor.com/NNhDpbFeeB.gif",
+                endpoint: "https://tenor.com/NNhDpbFeeB.gif",
                 caption: "Nope"
             },
             {
-                endpoints: "https://tenor.com/bWwr6.gif",
+                endpoint: "https://tenor.com/bWwr6.gif",
                 caption: "How long are we talking??"
             },
             {
-                endpoints: "https://tenor.com/8Nn8.gif",
+                endpoint: "https://tenor.com/8Nn8.gif",
                 caption: "Well I do need the money"
             },
         ]
@@ -125,19 +125,19 @@ const promptsData = [
         text: "What would you do if you woke up and found out you were six years old again?",
         gifs: [
             {
-                endpoints: "https://tenor.com/bWT02.gif",
+                endpoint: "https://tenor.com/bWT02.gif",
                 caption: "Cry tears of joy because you don’t pay bills anymore"
             },
             {
-                endpoints: "https://tenor.com/bWT02.gif",
+                endpoint: "https://tenor.com/bWT02.gif",
                 caption: "Cry because your adult life was lit"
             },
             {
-                endpoints: "https://tenor.com/l4cgpNHUZMB.gif",
+                endpoint: "https://tenor.com/l4cgpNHUZMB.gif",
                 caption: "Raid the kitchen because there’s finally food"
             },
             {
-                endpoints: "https://tenor.com/LLjd.gif",
+                endpoint: "https://tenor.com/LLjd.gif",
                 caption: "Get your siblings back for something they did years ago"
             },
         ]
@@ -146,19 +146,19 @@ const promptsData = [
         text: "Your friends set you up on a blind date with your ex. When you find out what do you do?",
         gifs: [
             {
-                endpoints: "https://tenor.com/bkm62.gif",
+                endpoint: "https://tenor.com/bkm62.gif",
                 caption: "Hell Naw"
             },
             {
-                endpoints: "https://tenor.com/bhjZw.gif",
+                endpoint: "https://tenor.com/bhjZw.gif",
                 caption: "Not taday"
             },
             {
-                endpoints: "https://tenor.com/be9ya.gif",
+                endpoint: "https://tenor.com/be9ya.gif",
                 caption: "I missed them"
             },
             {
-                endpoints: "https://tenor.com/lmf9tOiWxGd.gif",
+                endpoint: "https://tenor.com/lmf9tOiWxGd.gif",
                 caption: "I’m a ghost"
             },
         ]
@@ -167,19 +167,19 @@ const promptsData = [
         text: "Jon joins your breakout room with his Apple Vision Pro's on... What is he watching? ",
         gifs: [
             {
-                endpoints: "https://tenor.com/vDFt5HxVzTt.gif",
+                endpoint: "https://tenor.com/vDFt5HxVzTt.gif",
                 caption: "Try to fix it before your parents see"
             },
             {
-                endpoints: "https://tenor.com/3SPZ.gif",
+                endpoint: "https://tenor.com/3SPZ.gif",
                 caption: "Try to fix it before your parents see"
             },
             {
-                endpoints: "https://tenor.com/eJ1HOhgUUL9.gif",
+                endpoint: "https://tenor.com/eJ1HOhgUUL9.gif",
                 caption: "Try to fix it before your parents see"
             },
             {
-                endpoints: "https://tenor.com/bwLpb.gif",
+                endpoint: "https://tenor.com/bwLpb.gif",
                 caption: "Try to fix it before your parents see"
             },
         ]
@@ -188,19 +188,19 @@ const promptsData = [
         text: "When the Barista mess up your name AGAIN!",
         gifs: [
             {
-                endpoints: "https://tenor.com/bQVsp.gif",
+                endpoint: "https://tenor.com/bQVsp.gif",
                 caption: "Try to fix it before your parents see"
             },
             {
-                endpoints: "https://tenor.com/bv6pl.gif",
+                endpoint: "https://tenor.com/bv6pl.gif",
                 caption: "Try to fix it before your parents see"
             },
             {
-                endpoints: "https://tenor.com/bZVTK.gif",
+                endpoint: "https://tenor.com/bZVTK.gif",
                 caption: "Try to fix it before your parents see"
             },
             {
-                endpoints: "https://tenor.com/bHxnv.gif",
+                endpoint: "https://tenor.com/bHxnv.gif",
                 caption: "Try to fix it before your parents see"
                 
             },
