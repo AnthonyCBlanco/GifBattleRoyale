5fa5f4746a7b9686e067bfcfc4b3e4f87dc1f38c
Anthony Blanco <anthony200527@outlook.com>
2024-04-04 18:32:08 -0700

    seedData template

---
+++ server/seed/seedData.js
@@ -0,0 +1,105 @@
+
+const propmtData = [
+    {
+        text: "THis is the prompt",
+        gifs: [
+            {
+                endpoints: "https://media1.tenor.com/m/b8gWCDKua2oAAAAd/side-eye.gif"
+            },
+            {
+                endpoints: "https://media1.tenor.com/m/b8gWCDKua2oAAAAd/side-eye.gif"
+            },
+            {
+                endpoints: "https://media1.tenor.com/m/b8gWCDKua2oAAAAd/side-eye.gif"
+            },
+            {
+                endpoints: "https://media1.tenor.com/m/b8gWCDKua2oAAAAd/side-eye.gif"
+            },
+        ]
+    }
+    {
+        text: "THis is the prompt",
+        gifs: [
+            {
+                endpoints: ""
+            },
+            {
+                endpoints: ""
+            },
+            {
+                endpoints: ""
+            },
+            {
+                endpoints: ""
+            },
+        ]
+    },
+    {
+        text: "THis is the prompt",
+        gifs: [
+            {
+                endpoints: ""
+            },
+            {
+                endpoints: ""
+            },
+            {
+                endpoints: ""
+            },
+            {
+                endpoints: ""
+            },
+        ]
+    },
+    {
+        text: "THis is the prompt",
+        gifs: [
+            {
+                endpoints: ""
+            },
+            {
+                endpoints: ""
+            },
+            {
+                endpoints: ""
+            },
+            {
+                endpoints: ""
+            },
+        ]
+    },
+    {
+        text: "THis is the prompt",
+        gifs: [
+            {
+                endpoints: ""
+            },
+            {
+                endpoints: ""
+            },
+            {
+                endpoints: ""
+            },
+            {
+                endpoints: ""
+            },
+        ]
+    },
+    {
+        text: "THis is the prompt",
+        gifs: [
+            {
+                endpoints: ""
+            },
+            {
+                endpoints: ""
+            },
+            {
+                endpoints: ""
+            },
+            {
+                endpoints: ""
+            },
+        ]
+    }
+]
