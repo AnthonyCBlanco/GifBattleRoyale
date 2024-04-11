d46c65a99d4498b7d81e31f6d272882019c7a763
Anthony Blanco <anthony200527@outlook.com>
2024-04-10 18:19:35 -0700

    fixed endpage again

--- client/src/endpage/endpage.jsx
+++ client/src/endpage/endpage.jsx
@@ -1,22 +1,22 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import MainNavbar from '../components/navbar';
 import { Container } from 'react-bootstrap';
 import { Link } from 'react-router-dom'; // Added useHistory for navigation
 import './endpage.css';
 
 const EndPage = () => {
-    const [canPlay, setCanPlay] = useState(true);
-    const history = useHistory(); // Using useHistory for navigation
+    // const [canPlay, setCanPlay] = useState(true);
+    // const history = useHistory(); // Using useHistory for navigation
 
-    useEffect(() => {
-        const lastPlayTimestamp = localStorage.getItem('lastPlayTimestamp');
-        const currentTime = new Date().getTime();
-        const twentyFourHours = 24 * 60 * 60 * 1000;
+    // useEffect(() => {
+    //     const lastPlayTimestamp = localStorage.getItem('lastPlayTimestamp');
+    //     const currentTime = new Date().getTime();
+    //     const twentyFourHours = 24 * 60 * 60 * 1000;
 
-        if (lastPlayTimestamp && currentTime - lastPlayTimestamp < twentyFourHours) {
-            setCanPlay(false);
-        }
-    }, []);
+    //     if (lastPlayTimestamp && currentTime - lastPlayTimestamp < twentyFourHours) {
+    //         setCanPlay(false);
+    //     }
+    // }, []);
 
     const handlePlayAgain = () => {
         localStorage.setItem('lastPlayTimestamp', new Date().getTime());
@@ -31,14 +31,13 @@ const EndPage = () => {
                 <p className="score-text mb-5">
                     Check out the <Link to="/leaderboard">leaderboard</Link> to see how others did!
                 </p>
-                {canPlay ? (
+                
                     <button onClick={handlePlayAgain} className="btn btn-primary">Play Again</button>
-                ) : (
                     <div>
                         <h2>Please come back tomorrow to play again!</h2>
                         <img src="https://media1.tenor.com/m/t2oF_ayMhg0AAAAd/try-again-tomorrow-mr-miyagi.gif" alt="Come Back Tomorrow" className="rounded mx-auto d-block" />
                     </div>
-                )}
+                
             </Container>
         </div>
     );
