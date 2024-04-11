c86d2702043558acd91d9f237cafab986625107d
marialwj <mariaa.georgy@gmail.com>
2024-04-10 17:51:23 -0700

    endpage final

--- client/src/endpage/endpage.jsx
+++ client/src/endpage/endpage.jsx
@@ -1,29 +1,26 @@
 import React, { useEffect, useState } from 'react';
-
-import { QUERY_ME } from '../utils/queries';
-import { useQuery } from '@apollo/client';
-import { Container, Navbar, Nav } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
 import MainNavbar from '../components/navbar';
+import { Container } from 'react-bootstrap';
+import { Link, useHistory } from 'react-router-dom'; // Added useHistory for navigation
+import './endpage.css';
 
 const EndPage = () => {
     const [canPlay, setCanPlay] = useState(true);
+    const history = useHistory(); // Using useHistory for navigation
 
     useEffect(() => {
-        // Check if the user can play again based on the timestamp stored in localStorage
         const lastPlayTimestamp = localStorage.getItem('lastPlayTimestamp');
         const currentTime = new Date().getTime();
-        const twentyFourHours = 24 * 60 * 60 * 1000; // 24 hours in milliseconds
+        const twentyFourHours = 24 * 60 * 60 * 1000;
 
         if (lastPlayTimestamp && currentTime - lastPlayTimestamp < twentyFourHours) {
-            setCanPlay(false); // If less than 24 hours have passed, disable the play button
+            setCanPlay(false);
         }
     }, []);
 
     const handlePlayAgain = () => {
-        // Redirect to gameplay page and set the timestamp
-        // localStorage.setItem('lastPlayTimestamp', new Date().getTime());
-        window.location.href = '/play';
+        localStorage.setItem('lastPlayTimestamp', new Date().getTime());
+        history.push('/play'); 
     }
 
     return (
@@ -31,20 +28,20 @@ const EndPage = () => {
             <MainNavbar />
             <Container className="text-center mt-5">
                 <h1 className="mb-4">Thank You for Playing!</h1>
-                <h2 className="score-text mb-5">
-                    Check out the leaderboard to see how others did!
-                </h2>
+                <p className="score-text mb-5">
+                    Check out the <Link to="/leaderboard">leaderboard</Link> to see how others did!
+                </p>
                 {canPlay ? (
                     <button onClick={handlePlayAgain} className="btn btn-primary">Play Again</button>
                 ) : (
                     <div>
-                        <h3>Please come back tomorrow to play again!</h3>
-                        <img src="https://media1.tenor.com/m/t2oF_ayMhg0AAAAd/try-again-tomorrow-mr-miyagi.gif" className="rounded mx-auto d-block" alt="Come Back Tomorrow" />
+                        <h2>Please come back tomorrow to play again!</h2>
+                        <img src="https://media1.tenor.com/m/t2oF_ayMhg0AAAAd/try-again-tomorrow-mr-miyagi.gif" alt="Come Back Tomorrow" className="rounded mx-auto d-block" />
                     </div>
                 )}
             </Container>
         </div>
     );
-}
+};
 
 export default EndPage;
